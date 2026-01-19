7f0ffb63e9cc2215c1f15f3f1640a8e2bfed032e
sabbirHossain <sabbirshnt@gmail.com>
2026-01-19 19:06:40 +0600

    done

--- src/hooks/bonus.js
+++ src/hooks/bonus.js
@@ -2,17 +2,12 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 import { API } from "../api";
 import { AxiosSecure } from "../lib/AxiosSecure";
 
-export const useBonusStatement = () => {
+export const useBonusQuery = (payload) => {
   return useQuery({
-    queryKey: ["bonus-statement"],
+    queryKey: ["bonus", payload],
     queryFn: async () => {
-      const { data } = await AxiosSecure.post(API.bonus, {
-        type: "viewStatement",
-      });
-
-      if (data?.success) {
-        return data?.result;
-      }
+      const { data } = await AxiosSecure.post(API.bonus, payload);
+      return data;
     },
   });
 };
